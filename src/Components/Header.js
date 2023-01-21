859ebebf3f59a4bede330570a8e02b82e32c11b9
Bishesh <bishesh@gmail.com>
2023-01-21 16:33:27 +0545

    new components and lay out structures

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header d-flex d-md-flex justify-content-end">
+        <a href="https://twitter.com/" target="-blank">
+            <i class="fa-brands fa-twitter"></i>
+        </a>
+            
+        <a href="https://www.linkedin.com/" target="-blank">
+            <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+    </div>
+  )
+}
